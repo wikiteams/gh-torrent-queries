0bdb3fd78f182567afb82059501cd1fe7c8386d8
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-05-20 13:09:17 +0200

    Submitted queries for getting follow-followee relation and team members

---
+++ followanie-2013.sql
@@ -0,0 +1,1 @@
+select u.login as who_is_followed, z.login as follower, f.created_at as date_when_followed from followers f, users u, users z where (f.user_id = u.id) and (f.follower_id = z.id) and (f.created_at between '2013-01-01 00:00:01' and '2013-12-31 23:59:59') order by date_when_followed asc
